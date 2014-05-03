f20672626e89bc6f9001e97835fdfd8e18123ee9
fabianhu <fabianwho@gmx.net>
2014-05-03 12:09:40 +0200

    updated descriptions

--- Mavrx-DX4e/Mavrx-DX4e.c
+++ Mavrx-DX4e/Mavrx-DX4e.c
@@ -1,7 +1,26 @@
 /*
 * Mavrx_DX4e.c
 *
-* Created: 01/09/2013 23:43:32
+fabianhu-dx4e
+
+Custom firmware for the Spektrum DX4e (may work on DX5e)
+
+The fabianhu version is focused on blade nano flying without expensive RC.
+
+Set the "mix" dip switch to on for "Nano mode"
+
+DSMX only for now, but see comments for other frame formats.
+
+Aux is throttle hold, Bind is the kill switch.
+
+Hold bind at turning on for bind mode. Press Bind for "Kill engine", unlock with Aux. Hold bind and toggle rate 4x = range test. Hold bind and toggle rate 10x = learn stick center (center all sticks and trim in advance)
+
+To download the new Firmware, set all dip-switches to the upper position, otherwise the SPI lines are connected to GND. Leave the fuses, as they are.
+
+If you manage to download the original Firmware, let me know (the lock bit will be set, that's for sure)
+
+
+* Original Created: 01/09/2013 23:43:32
 *  Author: Yuan
 */
 
